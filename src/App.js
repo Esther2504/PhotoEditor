f7a035fab9c1970c92539e21944387ff95e90a00
Esther2504 <esther252000@gmail.com>
2023-02-04 13:05:52 +0100

    seperate files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import { useState } from "react";
 import styled from "styled-components";
+import ImageArea from "./ImageArea";
 import upload from "./upload-icon.svg";
 
 function App() {
-const [image, setImage] = useState()
-
   return (
-   <Wrapper>
-<Input type="file" accept="image/png, image/jpeg, image/svg" onInput={setImage} />
-<img src={upload} />
-   </Wrapper>
+<>
+<ImageArea />
+</>
   );
 }
 
 export default App;
 
-const Wrapper = styled.div`
-width: 800px;
-height: 500px;
-margin: 50px auto;
-background-color: white;
-border-radius: 10px;
-box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
-`
-
-const Input = styled.input`
-
-`
