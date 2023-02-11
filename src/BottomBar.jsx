fc40e5e1c97c96b2653ea18da5bd68e57fa8be98
Esther2504 <esther252000@gmail.com>
2023-02-11 12:01:30 +0100

    visibility bottom bar

--- src/BottomBar.jsx
+++ src/BottomBar.jsx
@@ -16,21 +16,5 @@ width: 100%;
 height: 5vh;
 box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
 margin: 0 auto;
-`
-
-const Input = styled.input`
-  opacity: 0;
-  width: 40px;
-  height: 40px;
-  position: absolute;
-  cursor: pointer;
-`;
-
-const Upload = styled.img`
-width: 40px;
-`
-const UploadArea = styled.div`
-width: 40px;
-// border: 1px solid black;
-cursor: pointer;
+background-color: white;
 `
